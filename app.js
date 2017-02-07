354cafbb6a6fb309f026e5d3b88a4425e7a0336a
Max Struever <MaxStruever@gmail.COM>
2017-02-07 16:14:22 +0100

    first few files and shortcuts

---
+++ app.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+console.log("Runing web server. Listening...");
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html',function(error,contents){
+                response.write(contents);
+    response.end();
+                });
+}).listen(8080);
