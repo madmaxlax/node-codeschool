ca9307edc7eeedd320de51cddc458d564e893838
Struever <Maxwell.C.Struever@shell.com>
2017-02-09 15:43:38 +0100

    multi user chat works

--- socketio.js
+++ socketio.js
@@ -8,8 +8,18 @@ io.on('connection', function (client) {
 
     client.on('messages', function (data) {
         console.log(data);
-        client.emit("messages", { hello: 'received ' + data });
+//        client.emit("messages", { hello: 'received ' + data });
+        var nickname = client.nickname;
+        client.emit("messages", { hello: nickname+': ' + data });
+        client.broadcast.emit("messages", { hello: 'nickname: ' + data });
+        
+    });
+    client.on('join', function(name){
+        client.nickname = name;
+        console.log(name+ " joined");
 
+        client.emit("messages", { hello: 'new user ' + name + ' joined' });        
+        client.broadcast.emit("messages", { hello: 'new user ' + name + ' joined' });        
     });
 });
 app.get('/', function (req, res) {
